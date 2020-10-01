62aeb58dd09ffba1efdb96f70c44d512806099fe
nami7733 <68311123+nami7733@users.noreply.github.com>
2020-10-01 15:51:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000;
 
